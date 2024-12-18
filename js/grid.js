cad2dc3ec7350aebe0c431bd7287999b65820fb7
FlosMortis <b.makzar@yandex.ru>
2024-12-18 20:18:28 +0100

    Test Husky hook

--- js/grid.js
+++ js/grid.js
@@ -2,7 +2,7 @@ const grid = document.createElement('div');
 grid.className = 'grid';
 document.body.appendChild(grid);
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener("keydown", (event) => {
     if (event.ctrlKey && event.code === 'KeyG') {
         grid.classList.toggle('grid_visible');
     }
